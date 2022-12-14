2a8a7ec1ca894f587905a2f24fee22cdc204dcaa
Levi Lutz <levi6900@icloud.com>
2022-12-13 22:20:15 -0500

    Add test endpoint for words api

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,11 @@ app.post("/images", async (request, response) => {
     response.render("images")
 })
 
+app.get("/test", async (req, resp) => {
+    const { type } = req.query;
+    resp.send(await utils.getWord(type));
+});
+
 app.use((_, response) => {
     response.status(404).send("Resource not found");
 });
